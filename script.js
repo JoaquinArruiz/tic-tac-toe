83866bbbfb7f7bfb86d342eda04b8e68d283c676
JoaquinArruiz <92789285+JoaquinArruiz@users.noreply.github.com>
2023-02-03 21:59:19 -0300

    added random starter

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ let player1 = playerFactory("player1", "❌","") // player 1
 let player2 = playerFactory("player2", "⭕️", "") // player 2
 let activePlayer = "" // auxiliar player
 
+const sleep = async (milliseconds) => {
+    await new Promise(resolve => {
+        return setTimeout(resolve, milliseconds)
+    });
+};
 
-const winningCondition = [
+const winningCondition = [ // all the posible winning combinations
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -24,64 +29,75 @@ const winningCondition = [
 
 
 
-
-
-const gameBoard = () => {
+const gameBoard = () => { // all the gameboard
 
     let board = [];
     for (let i = 0; i<9; i++) {
-        board.push('');
+        board.push(''); //making the array.length = 9
     }
     
-    board.forEach((cell, index) => {
+    board.forEach((cell, index) => { // creating the cells
         cell = document.createElement('div');
         cell.classList.add('cell')
         cell.addEventListener('click', function(){
             cell.classList.add(activePlayer.type)
-            if (!activePlayer.positions.includes(index)){
+            if (!activePlayer.positions.includes(index)){ // prevents the player array from storing one position twice
                 activePlayer.positions.push(index);
             }
-            cell.innerText = activePlayer.marker
-            checkWinner()
-            nextPlayer()
-            console.log(activePlayer.positions)
-            cell.style.pointerEvents = 'none';
+            cell.innerText = activePlayer.marker // changes the cell text to the one in the playerFactory.type
+            checkWinner() // checks if there is any winner after any move
+            nextPlayer() // swap to the next player after any move
+            console.log(activePlayer.positions) // auxiliar print REMOVE IN THE FINAL VERSION
+            cell.style.pointerEvents = 'none'; // remove event listener after click
         }) 
         
-        
+
         cell.innerText = index
 
-
-        gameboard.appendChild(cell);
+        
+        gameboard.appendChild(cell); // pushing the cell to the board
+        return cell
     })
     
 }
 
 
-function checkWinner(){
+function checkWinner(){ // checks the winner if any of the win conditions is accomplished
     winningCondition.forEach(condition =>{
         if (activePlayer.positions.includes(condition[0]) && activePlayer.positions.includes(condition[1]) && activePlayer.positions.includes(condition[2])){
-            return console.log('winner is ', activePlayer.type)
+            console.log('winner is ', activePlayer.type)
         }
     })
 }
 
-function nextPlayer(){
+function nextPlayer(){ //swaps the player
     if (activePlayer.type == player2.type){
         player2 = activePlayer // updates player to the active player before changin it
-        activePlayer = player1
-        console.log('changin to 1player')
-        return activePlayer;
+        activePlayer = player1 // swaps the auxiliar to the next player
+        console.log('changin to 1player') // auxiliar print REMOVE IN THE FINAL VERSION
+        return activePlayer; // returns the (before "next player") current player
     }
-    if (activePlayer.type == player1.type){
-        player1 = activePlayer // updates player to the active player before changin it
+    if (activePlayer.type == player1.type){ //the same as above but for player 2
+        player1 = activePlayer
         activePlayer =player2
         console.log('changin to 2player')
         return activePlayer;
     }
 }
 
-activePlayer = player1
+function randomStart(max){
+    starting = Math.floor(Math.random() * max)
+    console.log(starting)
+    if (starting == 0){
+        activePlayer = player1
+    }
+    else {
+        activePlayer = player2
+    }
+}
+
+randomStart(2)
+console.log(activePlayer.type)
 gameBoard()
 
 
