f1666f5754227ba865c3f9be9bb9644ea80be9d0
JoaquinArruiz <92789285+JoaquinArruiz@users.noreply.github.com>
2023-02-03 21:05:30 -0300

    updated javascript code, now support 1 player

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-
 let gameboard = document.createElement('div');
 gameboard.classList.add('gameboard')
 document.body.appendChild(gameboard)
 
-const playerFactory = (type, marker) => {
-    return {type, marker}
+const playerFactory = (type, marker, positions) => {
+    return {type, marker, positions: []}
 }
+let player1 = playerFactory("player1", "x","")
+let player2 = playerFactory("player", "o", "")
+let activePlayer = ""
+
+
+const winningCondition = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+
+
+
+
 
 const gameBoard = () => {
 
@@ -17,12 +36,36 @@ const gameBoard = () => {
     board.forEach((cell, index) => {
         cell = document.createElement('div');
         cell.classList.add('cell')
+        cell.addEventListener('click', function(){
+            cell.classList.add('marked')
+            if (!player1.positions.includes(index)){
+                player1.positions.push(index);
+            }
+            cell.innerText = activePlayer.marker
+            checkWinner()
+
+            console.log(player1.positions)
+        }) 
+        
         
         cell.innerText = index
         gameboard.appendChild(cell);
     })
+    
 }
 
+
+function checkWinner(){
+    winningCondition.forEach(condition =>{
+        if (activePlayer.positions.includes(condition[0]) && activePlayer.positions.includes(condition[1]) && activePlayer.positions.includes(condition[2])){
+            return console.log('winner is ', activePlayer.type)
+        }
+    })
+}
+
+
+
+activePlayer = player1
 gameBoard()
 
 
